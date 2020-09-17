1218a8bd38985500c5de681f069784033d17b6f6
Toni Magni <amagni@topsOrtho.com>
2020-09-17 18:15:30 -0400

    Added empty terms

--- dicom_photo/m_orthodontic_photograph_types.py
+++ dicom_photo/m_orthodontic_photograph_types.py
@@ -4,18 +4,20 @@ from pydicom.dataset import Dataset
 import dicom_photo.m_dental_acquisition_context_module
 
 class OrthodonticPhotographTypes(object):
-    EV01 = []
-    EV02 = []
-
     def __init__(self):
-        self.EV01 = [self.EO,self.RP,self.LR,self.CO]
-        self.EV02 = [self.EO,self.RP,self.LR,self.CR]
+        self.EV01 = [self._EO,self._RP,self._LR,self._CO]
+        self.EV02 = [self._EO,self._RP,self._LR,self._CR]
+        self.EV03 = [self._EO,self._RP,self._LC,self._CO]
+        self.EV04 = [self._EO,self._RP,self._LC,self._CR]
+        self.EV05 = [self._EO,self._RP,self._FS,self._CO]
+        self.EV06 = [self._EO,self._RP,self._FS,self._CR]
+        self.EV07 = [self._EO,self._RP,self._MD,self._PF]
 
 
-    def EO(self,dataset):
+    def _EO(self,dataset):
         pass
 
-    def RP(self, dataset):
+    def _RP(self, dataset):
         """ Right Profile
         """
         self._face(dataset)
@@ -23,30 +25,223 @@ class OrthodonticPhotographTypes(object):
         dataset.AcquisitionView = self._get_sct_code_sequence('30730003','Sagittal (qualifier value)')
         dataset.PatientOrientation = ['A','F'] # Anterior, Foot
 
-    def LP(self, dataset):
-        """ Right Profile
+    def _LP(self, dataset):
+        """ Left Profile
         """
         self._face(dataset)
         dataset.ImageLaterality = 'L'
         dataset.PatientOrientation = ['P','F'] # Anterior, Foot
 
-    def FF(self, dataset):
+    def _FF(self, dataset):
         self._face(dataset)
         dataset.ImageLaterality = 'B' # Both
         dataset.PatientOrientation = ['R','F'] # Right, Foot
 
-    def LR(self,dataset):
+    def _FS(self, dataset):
+        """ Full Smile
+        """
+        self._face(dataset)
+        dataset.ImageLaterality = 'B' # Both
+
+    def _LC(self,dataset):
+        """ Lips Closed
+        """
+        dataset.FunctionalCondition = self._get_sct_code_sequence('787607005', 'Lips closed')
+
+    def _LR(self,dataset):
         """ Lips Relaxed
         """
         dataset.FunctionalCondition = self._get_sct_code_sequence('745165', 'Lips relaxed')
 
-    def CO(self,dataset):
+    def _CO(self,dataset):
         dataset.OcclusalRelationship = self._get_sct_code_sequence('110320000',
             'Centric occlusion (observable entity)')
 
-    def CR(self,dataset):
+    def _CR(self,dataset):
         dataset.OcclusalRelationship = self._get_sct_code_sequence('736783005',
             'Centric relation (observable entity)')
+
+    def _MD(self,dataset):
+        """ Mandibular 
+        """
+        dataset.OcclusalRelationship = self._get_sct_code_sequence('736783005',
+            'Centric relation (observable entity)')
+
+    def _MX(self,dataset):
+        """ Maxillary
+        """
+        pass
+
+    def _MO(self,dataset):
+        """ Mouth Open
+        """
+        pass
+
+    def _TA(self,dataset):
+        """ Teeth Apart
+        """
+        pass
+
+    def _OJ(self,dataset):
+        """ Showing Overjet
+        """
+        pass
+
+    def _PF(self,dataset):
+        """ Mandible Postured Forward
+        """
+        pass
+
+    def _OF(self,dataset):
+        """ Other Face
+
+        some other view of the face; e.g., with the face tipped back, or from
+        above
+        """
+        pass
+
+    def _WM(self,dataset):
+        """ With Mirror
+
+        used for intraoral photographs only. Does not apply to extraoral
+        photographs
+        """
+        pass
+
+    def _WM_BC(self,dataset):
+        """ With Mirror But Corrected
+
+        flipped and/or rotated to appear as if it has been taken directly
+        without a mirror used for intraoral photographs only. Does not apply
+        to extraoral photographs
+        """
+        pass
+
+    def _RB(self,dataset):
+        """ Right Buccal
+
+        used to document posterior occlusion
+        """
+        pass
+
+    def _LB(self,dataset):
+        """ Left Buccal
+
+        used to document posterior occlusion
+        """
+        pass
+
+    def _RL(self,dataset):
+        """ Right Lateral
+
+        used to document the dental occlusion from the subject's right side
+        """
+        pass
+
+    def _LL(self,dataset):
+        """ Left Buccal
+
+        used to document the dental occlusion from the subject's left side
+        """
+        pass
+
+    def _FV(self,dataset):
+        """ Frontal View
+        """
+        pass
+
+    def _IV(self,dataset):
+        """ Inferior View
+
+        use IO.IV to show depth of bite and overjet from below
+        """
+        pass
+
+    def _SV(self,dataset):
+        """ Superior View
+        """
+        pass
+
+    def _45(self,dataset):
+        """ 45º View
+        """
+        pass
+
+    def _OV(self,dataset):
+        """ Occlusal View
+        """
+        pass
+
+    def _CS(self,dataset):
+        """ Close-up Smile
+        """
+        pass
+
+    def _OC(self,dataset):
+        """ Occlusal Cant
+        """
+        pass
+
+    def _FI(self,dataset):
+        """ Forensic Interest
+        """
+        pass
+
+    def _NW(self,dataset):
+        """ Nerve Weakness
+        """
+        pass
+
+    def _AN(self,dataset):
+        """ Anomalies
+        """
+        pass
+
+    def _FR(self,dataset):
+        """ Frenum
+        """
+        pass
+
+    def _PA(self,dataset):
+        """ Using Photo Accessory
+
+        such as a photo contraster providing a solid background or black
+        mirror. Can be appended to any intraoral (IO) view code as needed
+        """
+        pass
+
+    def _TT(self,dataset):
+        """ Tongue Thrust
+        """
+        pass 
+
+    def _IN(self,dataset):
+        """ Initial
+
+        refers to Initial time point, generally observation or pre-treatment
+        """
+        pass
+
+    def _P(self,dataset):
+        """ Progress
+
+        refers to Progress time point, i.e. progress photos
+        """
+        pass
+
+    def _F(self,dataset):
+        """ Final
+
+        refers to Final time point, generally when treatment is finished
+        """
+        pass
+    def _FU(self,dataset):
+        """ Follow-Up
+
+        refers to Follow-Up time point, generally taken to show
+        post-treatment changes
+        """
+        pass
 
     def _face(self,dataset):
         a_r_s = self._get_sct_code_sequence('302549007','Entire face (body structure)')

686c552e00c099e6d274941efd7720578921135e
Toni Magni <amagni@topsortho.com>
2021-02-19 10:40:34 -0500

    Clean up setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-"""
-DICOM Visible Light Photographic Image IOD
-
+""" A DICOM Implementation for Orthodontic Images
 """
 
 # Always prefer setuptools over distutils
@@ -18,10 +16,9 @@ import io
 import re
 
 
-
 init_py = io.open('dicom_photo/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
-metadata['doc'] = """ADICOM Visible Light Photographic Image IOD"""
+metadata['doc'] = """A DICOM Implementation for Orthodontic Images"""
 
 here = path.abspath(path.dirname(__file__))
 
@@ -29,78 +26,29 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
+# # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    
+
     python_requires='>=3',
-    
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
+
     name='dicom_photo',  # Required
 
-    # Versions should comply with PEP 440:
-    # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.1',  # Required
 
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
     description=metadata['doc'],  # Required
 
-    # This is an optional longer description of your project that represents
-    # the body of text which users will see when they visit PyPI.
-    #
-    # Often, this is the same as your README, so you can just read it in from
-    # that file directly (as we have already done above)
-    #
-    # This field corresponds to the "Description" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-optional
     long_description=long_description,  # Optional
 
-    # Denotes that our long_description is in Markdown; valid values are
-    # text/plain, text/x-rst, and text/markdown
-    #
-    # Optional if long_description is written in reStructuredText (rst) but
-    # required for plain-text or Markdown; if unspecified, "applications should
-    # attempt to render [the long_description] as text/x-rst; charset=UTF-8 and
-    # fall back to text/plain if it is not valid rst" (see link below)
-    #
-    # This field corresponds to the "Description-Content-Type" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
     long_description_content_type='text/markdown',  # Optional (see note above)
 
-    # This should be a valid link to your project's main homepage.
-    #
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
     url=metadata['url'],
 
-    # This should be your name or the name of the organization which owns the
-    # project.
     author=metadata['author'],
 
-    # This should be a valid email address corresponding to the author listed
-    # above.
     author_email=metadata['email'],
 
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see https://pypi.org/classifiers/
     classifiers=[  # Optional
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -120,71 +68,16 @@ setup(
         'Programming Language :: Python :: 3',
     ],
 
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
     keywords='DICOM VL Visible Light Photography Orthodontics',  # Optional
 
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
 
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'argparse',
         'pydicom',
         'pynetdicom',
         'pillow'],  # Optional
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-#     extras_require={  # Optional
-#         'dev': ['check-manifest'],
-#         'test': ['coverage'],
-#     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    #
-    # If using Python 2.6 or earlier, then these have to be included in
-    # MANIFEST.in as well.
-#     package_data={  # Optional
-#         'sample': ['package_data.dat'],
-#     },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-#     data_files=[('my_data', ['data/data_file'])],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
             'img2dicom=dicom_photo.__main__:main',
@@ -200,18 +93,10 @@ setup(
     # issues, where the source is hosted, where to say thanks to the package
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
-#     project_urls={  # Optional
-#         'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
-#         'Funding': 'https://donate.pypi.org',
-#         'Say Thanks!': 'http://saythanks.io/to/example',
-#         'Source': 'https://github.com/pypa/sampleproject/',
-#     },
-
-    # Call these classes after installation
-    # cmdclass={
-    #     'develop': PostDevelopCommand,
-    #     'install': PostInstallCommand,
-    # },
-
-
+    #     project_urls={  # Optional
+    #         'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+    #         'Funding': 'https://donate.pypi.org',
+    #         'Say Thanks!': 'http://saythanks.io/to/example',
+    #         'Source': 'https://github.com/pypa/sampleproject/',
+    #     },
 )

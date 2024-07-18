a8c17df7c606029f6f1ad4e6dbd56951b77ede1e
Toni Magni <email@antoniomagni.com>
2024-07-18 15:26:12 +0200

    Bump version: 0.3.9-dev -> 0.3.9

--- dicom4ortho/defaults.py
+++ dicom4ortho/defaults.py
@@ -8,7 +8,7 @@ import logging
 from pathlib import Path
 import importlib.resources as importlib_resources
 
-VERSION = '0.3.9-dev'
+VERSION = '0.3.9'
 PROJECT_NAME = 'dicom4ortho'
 __url__ = 'https://github.com/open-ortho/dicom4ortho'
 __author__ = 'Toni Magni'
